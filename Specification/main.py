8c7b3123f661f70b1b9c2e2009156ed0f117cd43
k3y6reak <k3y6reak@naver.com>
2019-09-09 13:12:10 +0900

    S1P3143002-11 [+] Add prepro_nosize_canceling code

--- Specification/main.py
+++ Specification/main.py
@@ -2,8 +2,16 @@ from data import PreProcessing
 
 
 def main():
+
+    # Make PreProcessing object
     pre_processing = PreProcessing()
+
+    # Get Train, Test data
     question_train, question_test, answer_train, answer_test = pre_processing.load_data()
+
+    # Remove special character
+    remove_noise_question_train, remove_noise_question_test, remove_noise_answer_train, remove_noise_answer_test\
+        = pre_processing.prepro_noise_canceling(question_train, question_test, answer_train, answer_test)
 
 
 if __name__ == '__main__':
