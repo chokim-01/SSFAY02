e160f246d41179dea20a02cddcdc95f3e410270a
k3y6reak <k3y6reak@naver.com>
2019-09-09 11:47:50 +0900

    S1P3143002-10 [+] Add load_csv function

--- Specification/data.py
+++ Specification/data.py
@@ -1,7 +1,8 @@
+import pandas as pd
+
 class PreProcessing:
-	def __init__(self, param1):
-		self.val = 0
-		self.param1 = param1
+	def __init__(self):
+		self.train_percent = 0.8
 
 	def pred_next_string(self):
 		return ""
@@ -28,4 +29,28 @@ class PreProcessing:
 		return ""
 
 	def load_data(self):
-		return ""
+		"""
+		:return: question_train, question_test, answer_train, answer_test
+		"""
+
+		# Get chatbot data
+		data_frame = pd.read_csv("./data_in/ChatBotData.csv")
+
+		# Get question data
+		question = data_frame[["Q"]].values
+
+		# Get answer data
+		answer = data_frame[["A"]].values
+
+		# Calculate percent
+		data_len = len(data_frame)
+		train_index = round(data_len * self.train_percent)
+
+		# Divide question, answer for train ans test
+		question_train = question[:train_index]
+		question_test = question[train_index:]
+
+		answer_train = answer[:train_index]
+		answer_test = answer[train_index:]
+
+		return question_train, question_test, answer_train, answer_test
