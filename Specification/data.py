b9517da3c3e6559ddfd61d773cc83e9727dc01b4
k3y6reak <k3y6reak@naver.com>
2019-09-09 09:49:44 +0900

    [+] Add base code

---
+++ Specification/data.py
@@ -0,0 +1,31 @@
+class PreProcessing:
+	def __init__(self, param1):
+		self.val = 0
+		self.param1 = param1
+
+	def pred_next_string(self):
+		return ""
+
+	def load_voc(self):
+		return ""
+
+	def make_voc(self):
+		return ""
+
+	def dec_target_processing(self):
+		return ""
+
+	def dec_output_processing(self):
+		return ""
+
+	def enc_processing(self):
+		return ""
+
+	def tokenizing_data(self):
+		return ""
+
+	def prepro_noise_canceling(self):
+		return ""
+
+	def load_data(self):
+		return ""
