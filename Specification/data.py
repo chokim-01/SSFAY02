fdac2bf6be4d0a1ac055120ad608c3f697310492
k3y6reak <k3y6reak@naver.com>
2019-09-10 10:56:13 +0900

    S1P3143002-13 [+] Add voca dictionary func and code

--- Specification/data.py
+++ Specification/data.py
@@ -5,6 +5,15 @@ import pandas as pd
 class PreProcessing:
 	def __init__(self):
 		self.train_percent = 0.8
+		self.question_and_answer = None
+		self.PAD = "<PAD>"
+		self.STD = "<SOS>"
+		self.END = "<END>"
+		self.UNK = "<UNK>"
+		self.PAD_INDEX = 0
+		self.STD_INDEX = 1
+		self.END_INDEX = 2
+		self.UNK_INDEX = 3
 
 	def pred_next_string(self):
 		return ""
@@ -13,7 +22,26 @@ class PreProcessing:
 		return ""
 
 	def make_voc(self):
-		return ""
+		voca_dictionary = {"<PAD>": 0, "<SOS>": 1, "<END>": 2, "<UNK>": 3}
+
+		idx = 4
+		for sentence in self.question_and_answer:
+
+			noise_cancel_sentence_0 = self.prepro_noise_canceling([sentence[0]])
+			noise_cancel_sentence_1 = self.prepro_noise_canceling([sentence[1]])
+
+			tokenize_sentence_0 = self.tokenize_data(noise_cancel_sentence_0)
+			tokenize_sentence_1 = self.tokenize_data(noise_cancel_sentence_1)
+
+			for token in tokenize_sentence_0[0]:
+				voca_dictionary[token] = idx
+				idx += 1
+
+			for token in tokenize_sentence_1[0]:
+				voca_dictionary[token] = idx
+				idx += 1
+
+		return voca_dictionary
 
 	def dec_target_processing(self):
 		return ""
@@ -21,7 +49,8 @@ class PreProcessing:
 	def dec_output_processing(self):
 		return ""
 
-	def enc_processing(self):
+	def enc_processing(self, message, word_dictionary):
+
 		return ""
 
 	def tokenize_data(self, message):
@@ -54,7 +83,7 @@ class PreProcessing:
 		message_len = len(message)
 
 		if message_len == 1:
-			message[0] = re.sub(remove_characters, "", message[0][0])
+			message[0] = re.sub(remove_characters, "", message[0])
 		elif message_len == 0:
 			print("Check message")
 		else:
@@ -70,6 +99,7 @@ class PreProcessing:
 
 		# Get chatbot data
 		data_frame = pd.read_csv("./data_in/ChatBotData.csv")
+		self.question_and_answer = data_frame[["Q", "A"]].values
 
 		# Get question data
 		question = data_frame[["Q"]].values
