0ae16fd14f0764094b0ba7f1f9a4d2fbcb20ac47
default <chokim159@naver.com>
2019-09-10 10:20:06 +0900

    S1P3143002-53 [+] Add web_server (flask_app.py) for test data input
    Former-commit-id: 79f7b3590a70bc55b574e088df808968013058d5

---
+++ Additional/Server/Web/flask_app.py
@@ -0,0 +1,226 @@
+from flask import Flask, jsonify, request
+from scipy.sparse import lil_matrix
+from flask_cors import CORS
+from konlpy.tag import Okt
+import pandas as pd
+import numpy as np
+import pickle
+import os
+import conn.conn as conn
+
+# Get file path
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_PATH = os.path.join(ROOT_PATH + "..\\..\\front", 'dist')
+
+app = Flask(__name__, static_folder=STATIC_PATH, static_url_path='')
+
+# Get ai model
+
+with open('./logistic_regression_model.clf', 'rb') as model:
+    logistic_regression_model = pickle.load(model)
+
+with open('./word_indices.clf', 'rb') as model:
+    word_indices = pickle.load(model)
+
+# Set CORS
+cors = CORS(app, resources={
+    r"/api/*": {"origin": "*"}
+})
+
+
+# Tokenize data
+def tokenize_data(data_row):
+    """
+    :param data_row:
+    :return: tokenize data ([word/morpheme, word/morpheme, word/morpheme, ...]
+    """
+    okt = Okt()
+
+    return ['/'.join(t) for t in okt.pos(data_row, norm=True, stem=True)]
+
+
+# Naive Bayes Model
+def load_naive_metion(mention):
+    global multinomial_nb_model, word_indices
+    mention_token_data = tokenize_data(mention)
+
+    x_mention = lil_matrix((1, len(word_indices) + 1), dtype=np.int64)
+    for token in mention_token_data:
+        word = token.split("/")[0]
+        if word in word_indices:
+            x_mention[0, word_indices[word]] = 1
+
+    return int(multinomial_nb_model.predict(x_mention)[0])
+
+
+# Logistic Regression Model
+def load_logistic_metion(mention):
+    global logistic_regression_model, word_indices
+    mention_token_data = tokenize_data(mention)
+
+    x_mention = lil_matrix((1, len(word_indices) + 1), dtype=np.int64)
+
+    for token in mention_token_data:
+        word = token.split("/")[0]
+        if word in word_indices:
+            x_mention[0, word_indices[word]] = 1
+
+    return int(logistic_regression_model.predict(x_mention)[0])
+
+
+# Set directory path for vue
+@app.route("/")
+def main():
+    return app.send_static_file("index.html")
+
+
+# Get comments
+@app.route("/api/get/comments", methods=["GET"])
+def get_comments():
+    cursor = conn.db().cursor()
+
+    cursor.execute("select * from comments order by num desc")
+    result = cursor.fetchall()
+
+    return jsonify(result)
+
+
+# Get comments Total Count
+@app.route("/api/get/page_count", methods=["POST"])
+def get_start_page_end_page():
+    cursor = conn.db().cursor()
+
+    page = int(request.form.get("page"))
+
+    sql = "select count(*) as cnt from comments"
+    cursor.execute(sql)
+
+    cnt_comments = cursor.fetchone()["cnt"]
+
+    final_page = int(cnt_comments / 10)
+    start_page = page - 2
+
+    if start_page <= 0:
+        start_page = 1
+
+    end_page = page + 3
+    if end_page >= final_page:
+        end_page = final_page
+
+    page_list = []
+    for i in range(start_page, end_page):
+        page_list.append(i)
+
+    page_data = {"page_list": page_list, "final_page": final_page}
+
+    return jsonify(page_data)
+
+
+# Get comment_page
+@app.route("/api/get/comments_page", methods=["POST"])
+def get_comments_page():
+    cursor = conn.db().cursor()
+
+    page = int(request.form.get("page"))
+    limit = (page - 1) * 10
+
+    sql = "select * from comments order by num desc limit %s, 10"
+    cursor.execute(sql, limit)
+    result = cursor.fetchall()
+
+    return jsonify(result)
+
+
+# Insert comments
+@app.route("/api/insert/comments", methods=["POST"])
+def comment_push():
+    db = conn.db()
+    cursor = db.cursor()
+
+    context = request.form.get("context")
+
+    # calc label
+    label = load_logistic_metion(context)
+    sql = "insert into comments(context, label) values(%s, %s, %s)"
+    cursor.execute(sql, (context, label))
+    db.commit()
+
+    return jsonify(label)
+
+
+# Edit comment label naive
+@app.route("/api/edit/label", methods=["POST"])
+def label_naive_edit():
+    db = conn.db()
+    cursor = db.cursor()
+
+    num = request.form.get("num")
+    label = request.form.get("label")
+
+    label = int(label)
+    if label == 1:
+        label = 0
+    else:
+        label = 1
+
+    sql = "update comments set label =%s where num=%s"
+    cursor.execute(sql, (label, num))
+    db.commit()
+    return ""
+
+
+# Del comment
+@app.route("/api/del/comments", methods=["POST"])
+def comment_del():
+    db = conn.db()
+    cursor = db.cursor()
+
+    num = request.form.get("num")
+
+    sql = "delete from comments where num = %s"
+    cursor.execute(sql, num)
+    db.commit()
+
+    return ""
+
+
+# Load data
+def load_data():
+    # Get data
+    train_data_frame = pd.read_csv("../../Data/ratings_train.txt", sep='\t').dropna()[["document", "label"]].values
+
+    # ndarray to list
+    # train_data_frame = train_data_frame.tolist()
+
+    return train_data_frame
+
+
+# Add 150,000 case to DB
+def add_testcase():
+    # Read test case
+    db = conn.db()
+    cursor = db.cursor()
+
+    test_data = load_data()
+    label = test_data[:, 1].tolist()
+    test_data = test_data.tolist()
+
+    # test_data = np.append(test_data, label)
+
+    for idx in range(len(test_data)):
+        test_data[idx].append(label[idx])
+
+    sql = "insert into comments(context, label_naive, label_logistic) values (%s, %s, %s)"
+    cursor.executemany(sql, test_data)
+    db.commit()
+
+    return ""
+
+
+def main():
+    # add_testcase()
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
