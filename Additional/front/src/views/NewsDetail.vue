613fd72b60e9df924051f80502aa0d125f3d2329
yeong <yex38144@naver.com>
2019-09-10 15:44:58 +0900

    [+] Add vuetify

---
+++ Additional/front/src/views/NewsDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="newsDetail">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newsDetail',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+#newsDetail {
+  background-color: #D8D8D8;
+}
+
+</style>
