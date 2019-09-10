04d24cb537906b3e748891675e4e0def89afa72d
k3y6reak <k3y6reak@naver.com>
2019-09-10 10:56:13 +0900

    S1P3143002-13 [+] Add voca dictionary func and code

--- Specification/main.py
+++ Specification/main.py
@@ -9,18 +9,9 @@ def main():
     # Get Train, Test data
     question_train, question_test, answer_train, answer_test = pre_processing.load_data()
 
-    # Remove special character
-    noise_cancel_question_train = pre_processing.prepro_noise_canceling(question_train)
-    noise_cancel_question_test = pre_processing.prepro_noise_canceling(question_test)
-    noise_cancel_answer_train = pre_processing.prepro_noise_canceling(answer_train)
-    noise_cancel_answer_test = pre_processing.prepro_noise_canceling(answer_test)
-
-    # Tokenize data
-    tokenize_question_train = pre_processing.tokenize_data(noise_cancel_question_train)
-    tokenize_question_test = pre_processing.tokenize_data(noise_cancel_question_test)
-    tokenize_answer_train = pre_processing.tokenize_data(noise_cancel_answer_train)
-    tokenize_answer_test = pre_processing.tokenize_data(noise_cancel_answer_test)
-
+    # Make voca_dictionary
+    voca_dictionary = pre_processing.make_voc()
+    print(voca_dictionary)
 
 
 
