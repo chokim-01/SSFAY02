8c91b78cdf629e666bc9b1dcac05d285bf7d8b9a
양동권 <ydk9557819@naver.com>
2019-09-10 14:14:34 +0900

    S1P3143002-16 [+] Add load_voc()

--- Specification/data.py
+++ Specification/data.py
@@ -1,18 +1,76 @@
 import pandas as pd
 import re
+import os
+
 from sklearn.model_selection import train_test_split
+from configs import DEFINES
 
 
 class PreProcessing:
 
 	def __init__(self):
-		self.val = 0
+		self.PAD = "<PAD>"
+		self.STD = "<SOS>"
+		self.END = "<END>"
+		self.UNK = "<UNK>"
+
+		self.PAD_INDEX = 0
+		self.STD_INDEX = 1
+		self.END_INDEX = 2
+		self.UNK_INDEX = 3
+
+		self.MARKER = [self.PAD, self.STD, self.END, self.UNK]
+
 
 	def pred_next_string(self):
 		return ""
 
+
 	def load_voc(self):
-		return ""
+
+		# dictionary data
+		voc_list = []
+
+		# create dictionary
+		if (not (os.path.exists(DEFINES.vocabulary_path))):
+			# get data
+			data_frame = pd.read_csv("./data_in/ChatBotData.csv", sep=',').dropna()[["Q", "A", "label"]].values
+			question = data_frame[:, [0]]
+			answer = data_frame[:, [1]]
+
+			data = []
+			data.extend(question)
+			data.extend(answer)
+
+			# normalization data
+			data = self.prepro_noise_canceling(data)
+
+			# tokenization data
+			words = []
+			for idx in data:
+				words.extend(self.tokenizing_data(idx))
+
+			# removing duplicated
+			words = list(set(words))
+
+			# set marker
+			words[:0] = self.MARKER
+
+			# create dictionary data
+			with open(DEFINES.vocabulary_path, 'w', encoding='utf-8') as voc_file:
+				for word in words:
+					voc_file.write(word+"\n")
+
+		# read voc file and save voc_list
+		with open(DEFINES.vocabulary_path, 'r', encoding='utf-8') as voc_file:
+			for line in voc_file:
+				voc_list.append(line.rstrip('\n'))
+
+		# create dictionary
+		char_to_idx, idx_to_char = self.make_voc(voc_list)
+
+		return char_to_idx, idx_to_char
+
 
 	def make_voc(self):
 		return ""
@@ -23,7 +81,24 @@ class PreProcessing:
 	def dec_output_processing(self):
 		return ""
 
-	def enc_processing(self):
+	def enc_processing(self, value, dictionary):
+		"""
+		seq_input_index : index data
+		seq_len : length of sentence
+		"""
+
+		seq_input_index = []
+		seq_len = []
+
+		# noise canceling`
+		value = self.prepro_noise_canceling(value)
+
+		for seq in value:
+
+
+			seq_index = []
+
+
 		return ""
 
 	def tokenizing_data(self, text):
@@ -31,10 +106,9 @@ class PreProcessing:
 		tokenized_data = []
 
 		# text tokenization
-		for idx in range(len(text)):
-			str_split = text[idx].split()
-			for word in str_split:
-				tokenized_data.append(word)
+		str_split = text.split()
+		for word in str_split:
+			tokenized_data.append(word)
 
 		return tokenized_data
 
@@ -65,10 +139,8 @@ class PreProcessing:
 
 def main():
 	data = PreProcessing()
-	train_q, train_a, test_q, test_a = data.load_data()
-	noise_canceled = data.prepro_noise_canceling(test_a)
-	tokenized_data = data.tokenizing_data(noise_canceled)
-	print(tokenized_data)
+	data.load_voc()
+
 
 if __name__ == '__main__':
     main()
