a854f2bc5033a22a3fbeb2594244c9b3ba4989f3
yeong <yex38144@naver.com>
2019-09-11 13:42:24 +0900

    S1P3143002-56 [+] Add News Detail page

---
+++ Additional/front/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NewsDetail/>
+</template>
+
+<script>
+import NewsDetail from './views/NewsDetail.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NewsDetail
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
