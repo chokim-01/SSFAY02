b7b6c1078f4c4950c4d361ad02b2de40559b1814
yeong <yex38144@naver.com>
2019-09-18 10:06:48 +0900

    S1P3143002-55 [+] Add Newstitle roll part
    Former-commit-id: 4d5d2b5d61c697036d6771f6234c06a77a910c1f [formerly 3218cca825f1b97c6a4df72832959d0d627dfc6a]
    Former-commit-id: 931c1c5aa501a8e51c92091342850fa1eb478f62

--- Additional/frontend/src/views/Home.vue
+++ Additional/frontend/src/views/Home.vue
@@ -4,16 +4,20 @@
     <v-flex xs12 sm12 my-5>
       <div class="header">
 
-        <img src="@/assets/logo.png" class="logo" />
+        <img src="@/assets/logo.png" class="logo" @click="MovePage('AboutUs')"/>
         <title>맥 짚는 AI</title>
 
-        <div class="navi">
-          <div class="text-xs-center">
-            <v-btn color="secondary" @click="MovePage('NewsList')"> News </v-btn>
-            <v-btn color="secondary" @click="MovePage('Search')"> Search </v-btn>
-            <v-btn color="secondary" @click="MovePage('AboutUs')"> About Us </v-btn>
-          </div>
-        </div>
+        <v-layout class="navi">
+            <v-flex xs12 sm6>
+              <v-btn class="headerBtn" @click="MovePage('NewsList')" flat> News </v-btn>
+              <v-btn class="headerBtn" @click="MovePage('Search')" flat> Search </v-btn>
+              <v-btn class="headerBtn" @click="MovePage('AboutUs')" flat> About Us </v-btn>
+            </v-flex>
+            <v-flex hidden-xs-only sm3></v-flex>
+            <v-flex xs12 sm3 id="realNewsTitle">
+                <vue-swimlane :words="newsTitle" :scale="1.3" :pauseOnHover="true"></vue-swimlane>
+            </v-flex>
+        </v-layout>
       </div>
     </v-flex>
   </v-layout>
@@ -40,13 +44,13 @@ export default {
   data() {
     return {
       NowPage: "AboutUs",
+      newsTitle : ["1. 나는 사람이다.", "2. 자소서 쓰기 싫어요!", "3. 오늘은 9월 16일", "4. 가나다라가나다라가나다라가나다라가나다라가나다라가나다라"]
     }
   },
 
   methods: {
     MovePage(location) {
       this.NowPage = location;
-      console.log(this.NowPage)
     }
   }
 
@@ -72,14 +76,26 @@ export default {
         margin-left: 10px;
     }
 }
+
 .app {
     font-size: 14px;
     color: #2f2f2f;
 }
+
 .navi {
     border-bottom: 2px solid #2f2f2f;
     border-top: 2px solid #2f2f2f;
-    padding: 12px 0;
+    text-align: left !important;
 }
 
+.headerBtn {
+  background: #00000000 !important;
+}
+
+#realNewsTitle {
+  font-family: 'Noto Serif KR', serif;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 10px 0;
+}
 </style>
