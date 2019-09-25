64cd687e9a4f19a6f724030f9c13a635a3cd7340
parkhr <vlhong94@gmail.com>
2019-09-25 16:41:09 +0900

    [+] Edit news_crawling.py, conn.py
    Former-commit-id: 57aec7481006d2c6f2c565a9f2c217ab58a39c64 [formerly 4f6aa030f9d0170197cc5720fd44f34a4d9af9e7]
    Former-commit-id: 1a7e2625f987e735a3296051bc0c1e275c23487c

--- Additional/crawling/conn/conn.py
+++ Additional/crawling/conn/conn.py
@@ -5,9 +5,5 @@ def db():
     return pymysql.connect(host='localhost', user='root', password='toor', db='ssafynews_ai', charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
 
 
-def db_hr():
-    return pymysql.connect(host='localhost', user='root', password='ssafy1', db='ssafynews_ai', charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
-
-
 def cursor():
     return db.cursor()
