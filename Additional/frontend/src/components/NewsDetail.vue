43648ef3b29c72c7a6d0de2b3ff9e36a251cb210
ini080@naver.com <ini080@naver.com>
2019-09-25 10:10:26 +0900

    [+] Update Import Data from DataBase [+] Add API get Tag & get Page Count
    Former-commit-id: 234dadf72821db8a68f5d950c646092ce082eb01 [formerly b20de195b54e98bfe4bdc1ecfa96077ebe9f72d8]
    Former-commit-id: 0e03020afa115218dd0751ca8adcc99ae1b724e3

--- Additional/frontend/src/components/NewsDetail.vue
+++ Additional/frontend/src/components/NewsDetail.vue
@@ -1,77 +1,109 @@
 <template>
-  <v-layout row wrap>
-    <v-flex class="posCenter" xs12 sm8 mb-2>
-        <v-layout row wrap pb-4>
-          <v-flex id="newspaperTitle" xs12 >
-            {{ detailTitle }}
-          </v-flex>
-          <v-flex class="text-xs-center" xs12 mb-4>
-            <v-chip v-for="idx in 6" :key="idx" color="red" outline> <!--  임시 -->
-              # 블라블라
-            </v-chip>
-          </v-flex>
-          <v-flex class="text-xs-right" xs12 px-4>{{dateFormmat}}</v-flex>
-          <v-flex id="newspaperContent" xs12 pa-3>
-            {{ detailContext }}
-          </v-flex>
-        </v-layout>
-    </v-flex>
-    <v-flex id="newspaperGraph" xs12 mb-5 pa-3>
-      <v-layout row wrap>
-        <v-flex xs12 sm1/>
-        <v-flex xs12 sm5>
-          <canvas id="PNChart" width="300" height="300"/>
+<v-layout row wrap>
+  <v-flex class="posCenter" xs12 sm8 mb-2>
+    <v-layout row wrap pb-4>
+      <v-flex id="newspaperTitle" xs12>
+        {{ detailTitle }}
+      </v-flex>
+      <v-flex class="text-xs-center" xs12 mb-4>
+        <v-chip v-for="idx in 6" :key="idx" color="red" outline>
+          <!--  임시 -->
+          # 블라블라
+        </v-chip>
+      </v-flex>
+      <v-flex class="text-xs-right" xs12 px-4>{{dateFormmat}}</v-flex>
+      <v-flex id="newspaperContent" xs12 pa-3>
+        {{ detailContext }}
+      </v-flex>
+    </v-layout>
+  </v-flex>
+  <v-flex id="newspaperGraph" xs12 mb-5 pa-3>
+    <v-layout row wrap>
+      <v-flex xs12 sm1 />
+      <v-flex xs12 sm5>
+        <canvas id="PNChart" width="300" height="300" />
+      </v-flex>
+      <v-flex xs12 sm4 mt-5>
+        <canvas id="TimeChart" width="300" height="300" />
+      </v-flex>
+    </v-layout>
+  </v-flex>
+  <v-flex class="posCenter" xs12 sm8 mb-5>
+
+    <v-card id="newsComment" flat>
+
+      <v-layout class="mh50" row wrap>
+        <v-flex xs3 sm2>
+          <h2>댓글 {{commentsCount}}개</h2>
         </v-flex>
-        <v-flex xs12 sm4 mt-5>
-          <canvas id="TimeChart" width="300" height="300"/>
+        <v-flex xs7 sm9></v-flex>
+        <v-flex xs2 sm1>
+          <v-btn @click="showLocalFunc()">지역감정보기</v-btn>
         </v-flex>
       </v-layout>
-    </v-flex>
-    <v-flex class="posCenter" xs12 sm8 mb-5>
-      <v-card id="newsComment" flat>
-        <h2>댓글 111개</h2>
-        <v-list>
-          <template v-for="tmpIdx in 5" class="cmtRow">
-              <v-layout class="mh50" :key="tmpIdx+'key'" row wrap>
-                <v-flex class="newsCommentRow" xs12 sm2><b>2019-09-19</b></v-flex>
-                <v-flex class="newsCommentRow" v-text="tmp_comment" xs10 sm9></v-flex>
-                <v-flex xs2 sm1><v-chip color="white" text-color="blue" label>
-                  <v-icon left>fas fa-thumbs-up</v-icon>
-                  <v-flex hidden-xs-only>긍정</v-flex>
-                </v-chip></v-flex>
-              </v-layout>
-              <v-divider />
-              <v-layout class="mh50" :key="tmpIdx+'key2'" row wrap>
-                <v-flex class="newsCommentRow" xs12 sm2><b>2019-09-19</b></v-flex>
-                <v-flex class="newsCommentRow" v-text="tmp_comment" xs10 sm9></v-flex>
-                <v-flex xs2 sm1><v-chip color="white" text-color="red" label>
-                  <v-icon left>fas fa-thumbs-down</v-icon>
-                  <v-flex hidden-xs-only>부정</v-flex>
-                </v-chip></v-flex>
-              </v-layout>
-            <v-divider />
-          </template>
-        </v-list>
-        <div class="commentPage">
-          <v-pagination
-          id="newsCommentPage"
-          v-model="commentPage"
-          :length="5"
-          color="blue"
-          prev-icon="fas fa-chevron-left"
-          next-icon="fas fa-chevron-right"
-          ></v-pagination>
+
+      <v-list id="newsCommentList">
+        <template v-for="comment in comments" class="cmtRow" v-if="showLocal">
+          <v-layout class="mh50" row wrap>
+            <v-flex class="newsCommentRow" xs12 sm2><b>{{comment.comment_time}}</b></v-flex>
+            <v-flex class="newsCommentRow" v-text="comment.comment_context" xs10 sm9></v-flex>
+            <v-flex xs2 sm1 v-if="comment.label_news == '0'">
+              <v-chip id="newsThumbs" text-color="red" label>
+                <v-icon right>fas fa-thumbs-down</v-icon>
+              </v-chip>
+            </v-flex>
+            <v-flex xs2 sm1 v-else>
+              <v-chip id="newsThumbs" text-color="blue" label>
+                <v-icon right>fas fa-thumbs-up</v-icon>
+              </v-chip>
+            </v-flex>
+          </v-layout>
+        </template>
+
+        <template v-for="comment in comments" class="cmtRow" v-if="!showLocal && comment.label_local =='0' ">
+          <v-layout class="mh50" row wrap>
+            <v-flex class="newsCommentRow" xs12 sm2><b>{{comment.comment_time}}</b></v-flex>
+            <v-flex class="newsCommentRow" v-text="comment.comment_context" xs10 sm9></v-flex>
+            <v-flex xs2 sm1 v-if="comment.label_news == '0'">
+              <v-chip id="newsThumbs" text-color="red" label>
+                <v-icon right>fas fa-thumbs-down</v-icon>
+              </v-chip>
+            </v-flex>
+            <v-flex xs2 sm1 v-else>
+              <v-chip id="newsThumbs" text-color="blue" label>
+                <v-icon right>fas fa-thumbs-up</v-icon>
+              </v-chip>
+            </v-flex>
+          </v-layout>
+        </template>
+
+
+      </v-list>
+
+      <!-- paging button -->
+      <v-layout class="mh50 paging" row wrap>
+
+        <div v-if="thisPage != 1" @click="getComments(1)">처음</div>
+
+        <div class="" v-for="i in pageList">
+          <div @click="getComments(i)" class="thisPageClass pageButton" v-if="i === thisPage">{{i}}</div>
+          <div @click="getComments(i)" class="pageButton" v-else>{{i}}</div>
         </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
+
+        <div v-model="finalPage" @click="getComments(finalPage)">마지막</div>
+
+      </v-layout>
+
+    </v-card>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
 import Chart from "chart.js";
 export default {
   name: "NewsDetail",
-  data () {
+  data() {
     return {
       detailTitle: this.$route.params.detailTitle,
       detailContext: this.$route.params.detailContext,
@@ -79,66 +111,144 @@ export default {
       detailNum: this.$route.params.detailNum,
       myDoughnutChart: null,
       doughnutData: null,
-      tmp_comment: "고통이 고통이라는 이유로 그 자체를 사랑하고 소유하려는 자는 없다.",
-      commentPage: 1
+      comments: [],
+      pageList: [],
+      thisPage: 1,
+      finalPage: 0,
+      commentsCount: 0,
+      positiveCount: 0,
+      localCount: 0,
+      commentTime: [],
+      showLocal: true,
+      commentPage: 1,
     }
   },
+  created() {
+    this.getComments(1);
+  },
   mounted() {
-    var docPNChart = document.getElementById("PNChart");
-    var pnChart = new Chart(docPNChart, {
-      type: "doughnut",
-      data: {
-        labels: ["긍정", "부정"],
-        datasets: [{
-          label: "# of Votes",
-          data: [72, 28],
-          backgroundColor: [
-            "#01A9DB",
-            "#FE2E64"
-          ]
-        }]
-      },
-      options: {
-        maintainAspectRatio: false
-      }
-    });
+    this.getCommentsInfo();
+    this.getCommentsTime();
 
-    var docTimeChart = document.getElementById("TimeChart").getContext("2d");
-    var TimeChart = new Chart(docTimeChart, {
-      type: "line",
-      data: {
-        labels: ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am"],
-        datasets: [{
-          label: "시간대별 댓글 작성 추이",
-          data: [20000, 14000, 12000, 15000, 18000, 19000, 22000,20000, 14000, 12000, 15000, 18000, 19000, 22000],
-          borderColor: "#FA5882",
-          backgroundColor: "#00000000",
-          type: "line",
-          pointRadius: 0,
-          fill: false,
-          lineTension: 0,
-          borderWidth: 2
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              /*    beginAtZero: true, */
-            }
+    setTimeout(() => {
+      var docPNChart = document.getElementById("PNChart");
+      var pnChart = new Chart(docPNChart, {
+        type: "doughnut",
+        data: {
+          labels: ["긍정", "부정"],
+          datasets: [{
+            label: "# of Votes",
+            data: [this.positiveCount, (this.commentsCount - this.positiveCount)],
+            backgroundColor: [
+              "#01A9DB",
+              "#FE2E64"
+            ]
           }]
+        },
+        options: {
+          maintainAspectRatio: false
         }
-      },
-    });
+      });
+
+      var docTimeChart = document.getElementById("TimeChart").getContext("2d");
+      var TimeChart = new Chart(docTimeChart, {
+        type: "line",
+        data: {
+          labels: ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am"],
+          datasets: [{
+            label: "시간대별 댓글 작성 추이",
+            data: this.commentTime,
+            borderColor: "#FA5882",
+            backgroundColor: "#00000000",
+            type: "line",
+            pointRadius: 0,
+            fill: false,
+            lineTension: 0,
+            borderWidth: 2
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                /*    beginAtZero: true, */
+              }
+            }]
+          }
+        },
+      });
+    }, 300);
+
   },
   computed: {
     dateFormmat() {
-      var dDate = this.detailDate+"";
-      return dDate.substring(0,4) +" - "+dDate.substring(4,6)+" - "+dDate.substring(6,8);
+      var dDate = this.detailDate + "";
+      return dDate.substring(0, 4) + " - " + dDate.substring(4, 6) + " - " + dDate.substring(6, 8);
+    }
+  },
+
+  methods: {
+    // Get Comment
+    getComments(page) {
+      const axios = require("axios");
+      let formData = new FormData();
+      this.thisPage = page;
+      this.getPages(this.thisPage);
+      formData.append("news_num", this.detailNum);
+      formData.append("page", this.thisPage);
+      axios.post("http://localhost:5000/api/get/comments", formData)
+        .then(res => {
+          this.comments = res.data;
+        })
+    },
+
+    // Get Final Page Number and Page Number List
+    getPages(page) {
+      const axios = require("axios");
+      var self = this;
+      var formData = new FormData();
+      this.thisPage = page;
+      formData.append("news_num", this.detailNum);
+      formData.append("page", this.thisPage);
+      axios.post("http://localhost:5000/api/get/page_count", formData)
+        .then(res => {
+          self.pageList = res.data.page_list;
+          self.finalPage = res.data.final_page;
+        });
+    },
+    //get label of Comments
+    getCommentsInfo() {
+      const axios = require("axios");
+      let formData = new FormData();
+      formData.append("news_num", this.detailNum);
+      axios.post("http://localhost:5000/api/get/comments_label", formData)
+        .then(res => {
+          this.commentsCount = res.data[0].comment_cnt;
+          this.positiveCount = res.data[0].news_positive;
+          this.localCount = res.data[0].comment_cnt;
+        })
+    },
+    // Get Chart Time
+    getCommentsTime() {
+      const axios = require("axios");
+      let formData = new FormData();
+      formData.append("news_num", this.detailNum);
+      axios.post("http://localhost:5000/api/get/comments_time", formData)
+        .then(res => {
+          this.commentTime = res.data;
+          for (var i = 0; i < res.data.length; i++) {
+            this.commentTime[i] = res.data[i].hour_cnt;
+          }
+        })
+    },
+    // Show Regional sentiment
+    showLocalFunc() {
+      this.showLocal = !this.showLocal
     }
   }
 }
 </script>
+
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Song+Myung&display=swap");
 @import url("https://fonts.googleapis.com/css?family=Noto+Serif+KR&display=swap");
@@ -168,7 +278,7 @@ export default {
   text-align: center;
 }
 
-#newspaperContent{
+#newspaperContent {
   font-family: "Noto Serif KR", serif;
   overflow-y: scroll;
   height: 520px;
@@ -184,7 +294,8 @@ export default {
   margin-top: 30px;
 }
 
-#PNChart, #TimeChart {
+#PNChart,
+#TimeChart {
   max-height: 300px;
 }
 
@@ -202,11 +313,19 @@ export default {
   heigth: auto;
   min-height: 300px;
   padding: 20px;
-  background-color: #00000000;
+  background-color: inherit;
+}
+
+#newsCommentList {
+  background-color: inherit;
+}
+
+#newsThumbs {
+  background-color: inherit;
 }
 
 .newsCommentRow {
-  padding:12px 0px;
+  padding: 12px 0px;
 }
 
 .cmtRow {
@@ -218,13 +337,10 @@ export default {
   min-height: 50px;
 }
 
-#newsComment * {
-  background-color: inherit !important;
-}
 
 .theme--light.v-pagination .v-pagination__item--active {
-    color: black !important;
-    border: 1px solid blue !important;
+  color: black !important;
+  border: 1px solid blue !important;
 }
 
 @media (max-width: 600px) {
@@ -242,11 +358,55 @@ export default {
     margin-left: 8%;
   }
 
-  #newspaperContent{
+  #newspaperContent {
     column-count: 2 !important;
     max-height: none;
     height: auto;
     overflow: visible;
+  }
+
+  .thisPageClass {
+    color: black;
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+
+  .pageButton {
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #f6f6f6));
+    background: -moz-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+    background: -webkit-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+    background: -o-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+    background: -ms-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+    background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#f6f6f6', GradientType=0);
+    background-color: #ffffff;
+    -moz-border-radius: 3px;
+    -webkit-border-radius: 3px;
+    border-radius: 3px;
+    border: 2px solid #dcdcdc;
+    display: inline-block;
+    cursor: pointer;
+    color: #826d73;
+    font-family: Arial;
+    font-size: 16px;
+    padding: 6px 9px;
+    text-decoration: none;
+  }
+
+  .pageButton:hover {
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f6f6f6), color-stop(1, #ffffff));
+    background: -moz-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+    background: -webkit-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+    background: -o-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+    background: -ms-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+    background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f6f6f6', endColorstr='#ffffff', GradientType=0);
+    background-color: #f6f6f6;
+  }
+
+  .pageButton:active {
+    position: relative;
+    top: 1px;
   }
 }
 </style>
