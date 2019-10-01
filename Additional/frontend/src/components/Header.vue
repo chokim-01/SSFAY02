e3472c553239ef089a882afbfc62c536f6260aab
gayeong <yex38144@naver.com>
2019-10-01 14:15:29 +0900

    [+] Edit news slide text to link
    Former-commit-id: 55f7081b86b44f3e94d167e4d4f9479e51e00d42 [formerly 97349ad3e23dccf14d1520ca4fcd62e362678ce0]
    Former-commit-id: 2737f84c33978da3a48dfd1bd4d55147e55f473b

--- Additional/frontend/src/components/Header.vue
+++ Additional/frontend/src/components/Header.vue
@@ -4,22 +4,26 @@
     <v-flex class="header" xs12 my-5 >
         <v-layout row wrap>
           <v-flex xs12 sm8>
-            <router-link to="/"><img src="@/assets/logo.png" class="logo"/></router-link>
-            <title>맥 짚는 AI</title>
+            <router-link to="/">
+              <img src="@/assets/logo.png" class="logo"/>
+              <title>俟翍日報</title>
+            </router-link>
           </v-flex>
           <v-flex class="searchPart1" xs4 sm1>
             <v-select
-              v-model="searchCat"
-              :items="selectCat"
+              v-model="searchCategory"
+              :items="selectCategory"
+              item-text="label"
+              item-value="value"
               label="검색 옵션"
               append-icon="fas fa-caret-down"
             >
             </v-select>
           </v-flex>
-          <v-flex class="searchPart2" v-if="searchCat=='날짜'" xs8 sm3>
+          <v-flex class="searchPart2" v-if="searchCategory=='date'" xs8 sm3>
             <v-text-field
             v-model="searchKey"
-            style="width: 90%; margin: 0 auto;"
+            class="posAuto90"
             append-outer-icon="fas fa-search"
             @click:append-outer="SearchNews()"
             @click.stop="menu = !menu"
@@ -27,8 +31,8 @@
             ></v-text-field>
             <v-menu
             v-model="menu"
-            offset-y
             min-width="290px"
+            offset-y
             >
               <template v-slot:activator="{ on }"></template>
               <v-date-picker
@@ -45,7 +49,7 @@
           <v-flex class="searchPart2" v-else xs8 sm3>
             <v-text-field
             v-model="searchKey"
-            style="width: 90%; margin: 0 auto;"
+            class="posAuto90"
             placeholder="검색어를 입력하세요."
             append-outer-icon="fas fa-search"
             @click:append-outer="SearchNews()"
@@ -54,12 +58,12 @@
         </v-layout>
         <v-layout class="navi" row wrap>
             <v-flex xs12 sm4>
-              <v-btn class="headerBtn" to="/" flat> News </v-btn>
-              <v-btn class="headerBtn" to="AboutUs" flat> About Us </v-btn>
+              <v-btn class="headerButton" to="/" flat> News </v-btn>
+              <v-btn class="headerButton" to="AboutUs" flat> About Us </v-btn>
 
               <!-- chatbot -->
-              <v-icon class="chatbotIcon" @click.stop="chk_view = !chk_view">far fa-comment-dots</v-icon>
-              <v-menu class="chat" v-model="chk_view" :close-on-content-click="false" :nudge-width="200" offset-y>
+              <v-icon class="chatbotIcon" @click.stop="check_view = !check_view">far fa-comment-dots</v-icon>
+              <v-menu class="chat" v-model="check_view" :close-on-content-click="false" :nudge-width="200" offset-y>
                 <template v-slot:activator="{ on }"></template>
 
                 <v-card>
@@ -69,9 +73,11 @@
               </v-menu>
 
             </v-flex>
-            <v-flex hidden-xs-only sm3></v-flex>
-            <v-flex xs12 sm5 id="realNewsTitle">
-                <vue-swimlane :words="newsTitle" :scale="1" :pauseOnHover="true"></vue-swimlane>
+            <v-flex hidden-xs-only sm4></v-flex>
+            <v-flex xs12 sm4 id="realNewsTitle" >
+              <div @click="moveDetailPage()">
+                <vue-swimlane :words="newsTitle" :scale="1" :pauseOnHover="true" ></vue-swimlane>
+              </div>
             </v-flex>
         </v-layout>
     </v-flex>
@@ -92,11 +98,17 @@ export default {
     return {
       NowPage: "AboutUs",
       newsTitle : [""],
+      newsAllInfo: [],
       searchKey : "",
-      searchCat: "전체",
-      selectCat: ['전체', '제목', '태그', '날짜'],
+      searchCategory: "All",
+      selectCategory: [
+        {label: "전체", value: "all"},
+        {label: "제목", value: "title"},
+        {label: "태그", value: "tag"},
+        {label: "날짜", value: "date"}
+      ],
       menu: false,
-      chk_view: false,
+      check_view: false,
       text: ""
     }
   },
@@ -110,12 +122,12 @@ export default {
     },
 
     SearchNews() {
-      if(this.searchCat == "전체"){
-        this.$store.state.searchCat = this.searchCat;
+      if(this.searchCategory == "all"){
+        this.$store.state.searchCategory = this.searchCategory;
         this.$store.state.searchKey = "nullValue";
-        this.$store.state.searchChk = true;
+        this.$store.state.searchCheck = true;
         this.searchKey = "";
-        this.searchCat = "";
+        this.searchCategory = "";
       }
       else {
         if(this.searchKey == "" ) {
@@ -123,14 +135,14 @@ export default {
           return false;
         }
         else {
-          if(this.searchCat=="날짜") {
+          if(this.searchCategory=="date") {
             this.searchKey = this.searchKey.substring(0,4) + this.searchKey.substring(5,7) + this.searchKey.substring(8,10)
           }
           this.$store.state.searchKey = this.searchKey;
-          this.$store.state.searchCat = this.searchCat;
-          this.$store.state.searchChk = true;
+          this.$store.state.searchCategory = this.searchCategory;
+          this.$store.state.searchCheck = true;
           this.searchKey = "";
-          this.searchCat = "";
+          this.searchCategory = "";
         }
       }
       if(location.pathname != "/"){
@@ -146,16 +158,22 @@ export default {
 
       if(month<10) month = "0" + month;
 
-      day-=7; // 나중에 지울것
+      /* 나중에 삭제해야함 */
+          year = "2019";
+          month = "09";
+          day = "22";
+      /* */
 
       let formData = new FormData();
-      formData.append("page", 1);
+      formData.append("page", 10);
       formData.append("date", year+""+month+""+day);
-      Server(this.$store.state.SERVER_URL).post("/api/get/news", formData)
+      Server(this.$store.state.SERVER_URL).post("/api/get/head_news", formData)
         .then(res => {
+          this.newsTitle = [];
           for(var newsIdx in res.data) {
             this.newsTitle.push((parseInt(newsIdx)+1)+". "+res.data[newsIdx].news_title);
           }
+          this.newsAllInfo = res.data;
         })
     },
     enter(){
@@ -185,14 +203,26 @@ export default {
       })
 
       this.text = ""
+    },
+    moveDetailPage() {
+      var e = window.event;
+      var titleClick = e.target || e.srcElement;
+      var eventNum = parseInt(titleClick.innerHTML.substring(0,1));
+
+      this.$store.state.oneNewsInfo = this.newsAllInfo[eventNum-1];
+
+      if(window.location.pathname != "/NewsDetail") {
+        this.$router.push({path: "NewsDetail"});
+      }
     }
   }
-
 }
 </script>
 
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Ma+Shan+Zheng&display=swap');
+
 .header {
     position: relative;
 
@@ -207,6 +237,7 @@ export default {
         font-size: 4rem;
         display: inline-block;
         margin-left: 10px;
+        color: black;
     }
 }
 
@@ -221,7 +252,7 @@ export default {
     text-align: left !important;
 }
 
-.headerBtn {
+.headerButton {
   background: #00000000 !important;
 }
 
@@ -230,6 +261,7 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   margin-top: 10px;
+  padding-right: 2%;
 }
 
 .searchPart1 {
@@ -239,6 +271,11 @@ export default {
 
 .searchPart2 {
   padding-top: 4px;
+}
+
+.posAuto90 {
+  width: 90%;
+  margin: 0 auto;
 }
 
 .v-btn--active:before, .v-btn:focus:before, .v-btn:hover:before {
@@ -289,7 +326,19 @@ export default {
     width: 40%;
   }
 }
+</style>
 
+<style>
 
+.vue-swimlane ul {
+  padding-left: 0px !important;
+}
 
+#realNewsTitle li {
+  width:100%;
+  white-space: nowrap;
+  overflow: hidden !important;
+  text-overflow: ellipsis !important;
+  text-align: left;
+}
 </style>
