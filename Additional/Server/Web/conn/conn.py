2f2ada824f05bafbd79e8e7f45c1fa53d81efbbd
default <chokim159@naver.com>
2019-10-28 10:15:57 +0900

    [+] Edit change news, politics to sports
    Former-commit-id: d11a19caef95e847bd93e285de90811adc671bed [formerly f7e85d5b57f25d9153fd42109c491ae9d9d1e5e5]
    Former-commit-id: 4b5123cf981afc5ffce5ec089e1d829397fc1e4b

--- Additional/Server/Web/conn/conn.py
+++ Additional/Server/Web/conn/conn.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def db():
-    return pymysql.connect(host='13.125.116.42', user='mac_ai', password='mac_ai!@', db='ssafynews_ai', charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+    return pymysql.connect(host='13.125.20.58', user='mac_ai', password='mac_ai!@', db='ssafynews_ai', charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
 
 
 def cursor():
